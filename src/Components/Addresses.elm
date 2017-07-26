46a486ff94a7ccc659c391848e76acf02ed22858
Edwin Bühler <edbuehler@icloud.com>
2017-07-26 14:06:16 +0200

    Adressen als Table

--- src/Components/Addresses.elm
+++ src/Components/Addresses.elm
@@ -4,12 +4,16 @@ module Components.Addresses exposing (..)
 
 import Bootstrap.Grid as Grid
 import Bootstrap.ListGroup as Listgroup
+import Bootstrap.Table as Table
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Models exposing (Model)
 import Msgs exposing (Msg)
 
+
 -- ? Hover Info ????
+
+
 pageAddresses : Model -> List (Html Msg)
 pageAddresses model =
     [ br [] []
@@ -17,8 +21,18 @@ pageAddresses model =
     , h1 [] [ text "Adressen" ]
     , Grid.row []
         [ Grid.col []
-            [ Listgroup.ul
-                (List.map listEntry (List.sortBy .title addresses )) -- (List.sortBy .title (List.map listEntry addresses)) -- List.sortBy getVal dicts
+            [ Table.simpleTable
+                ( Table.simpleThead
+                    [ Table.th [] [ text "Logo" ]
+                    , Table.th [] [ text "Name" ]
+                    , Table.th [] [ text "Beschreibung" ]
+                    , Table.th [] [ text "Ansprechpartner" ]
+                    , Table.th [] [ text "Adresse" ]
+                    , Table.th [] [ text "ePost" ]
+                    ]
+                , Table.tbody [ style [ ( "text-align", "left" ) ]]
+                    (List.map tableEntry (List.sortBy .title addresses))
+                )
             ]
         ]
     ]
@@ -29,6 +43,29 @@ logoPrefix =
     "logos/"
 
 
+tableEntry : { title : String, url : String, contact : String, address : String, email : String, description : String, logo : String } -> Table.Row msg
+tableEntry address =
+    Table.tr []
+        [ Table.td []
+            [ img
+                [ src (logoPrefix ++ address.logo)
+                , style [ ( "height", "32px" ) ]
+                ]
+                []
+            ]
+        , Table.td [] [ a [ href address.url ] [ text address.title ] ]
+        , Table.td [] [ text address.description ]
+        , Table.td [] [ text address.contact ]
+        , Table.td [] [ text address.address ]
+        , Table.td [] [ text address.email ]
+        
+        ]
+
+
+
+-- Listgroup.ul (List.map listEntry (List.sortBy .title addresses ))
+
+-- Obsolete
 listEntry : { title : String, url : String, contact : String, address : String, email : String, description : String, logo : String } -> Listgroup.Item msg
 listEntry address =
     Listgroup.li []
@@ -37,11 +74,12 @@ listEntry address =
             , style [ ( "height", "32px" ) ]
             ]
             []
-        , div [class "address-title"] [a [ href address.url ] [ text address.title ]]
-        , div [class "address-contact"]  [ text address.contact ]
-        , div [class "address-address"]  [ text address.address]
-        , div [class "address-email"]  [ text address.email]
-        , div [class "address-description"]  [ text address.description]
+            -- Was wenn keine url vorhanden ?
+        , div [ class "address-title" ] [ a [ href address.url ] [ text address.title ] ]
+        , div [ class "address-contact" ] [ text address.contact ]
+        , div [ class "address-address" ] [ text address.address ]
+        , div [ class "address-email" ] [ text address.email ]
+        , div [ class "address-description" ] [ text address.description ]
         ]
 
 
@@ -55,12 +93,157 @@ addresses =
       , logo = "birgelen.png"
       , description = "Birgelen im Netz"
       }
-    , { title = "Title"
+    , { title = "Dorffreunde Birgelen e.V."
+      , url = "http://dorffreunde-birgelen.de/"
+      , logo = "birgelen.png"
+      , contact = ""
+      , address = ""
+      , email = "info@dorffreunde-birgelen.de"
+      , description = ""
+      }
+      , { title = "F.C. Concordia 1914 Birgelen e.V. "
       , url = ""
       , logo = "birgelen.png"
-      , contact = "Ansprechpartner"
-      , address = "Adresse"
+      , contact = "Andreas Paulus"
+      , address = "Jahnstraße 7"
+      , email = ""
+      , description = ""
+      }
+      , { title = "Feuerwehrtauchverein Birgelen e.V."
+      , url = ""
+      , logo = "birgelen.png"
+      , contact = "Peter Thissen"
+      , address = "Lambertusstraße 55"
+      , email = ""
+      , description = ""
+      }
+      , { title = "Förderverein des Kindergartens St. Lambertus Birgelen"
+      , url = ""
+      , logo = "birgelen.png"
+      , contact = "Heinz-Dieter Ohlenforst"
+      , address = "Lambertusstraße 19"
+      , email = ""
+      , description = ""
+      }
+      , { title = "Freiwillige Feuerwehr Birgelen"
+      , url = ""
+      , logo = "birgelen.png"
+      , contact = "Dirk Müller"
+      , address = "Mittlerer Weg 23"
+      , email = ""
+      , description = ""
+      }
+      , { title = "Kath. Frauengemeinschaft Birgelen"
+      , url = ""
+      , logo = "birgelen.png"
+      , contact = "Irmgard Becker"
+      , address = "Auf dem Feldchen 6"
+      , email = ""
+      , description = ""
+      }
+      , { title = "Kirchenchor Cäcilie Birgelen"
+      , url = ""
+      , logo = "birgelen.png"
+      , contact = "Matthias Pastoors"
+      , address = "Rosenthaler Straße 78"
+      , email = ""
+      , description = ""
+      }
+      , { title = "Musikverein Eintracht Birgelen e.V."
+      , url = "http://www.musikverein-birgelen.de/"
+      , logo = "birgelen.png"
+      , contact = "Frank Kämpfer"
+      , address = "AdreHeinrich Giesen Straße 7se"
+      , email = ""
+      , description = ""
+      }
+      , { title = "Ortsring Birgelen"
+      , url = ""
+      , logo = "birgelen.png"
+      , contact = "Heinrich Thissen"
+      , address = "Lambertusstr. 91"
+      , email = ""
+      , description = ""
+      }
+      , { title = "Briefmarkenfreunde Birgelen e.V."
+      , url = ""
+      , logo = "birgelen.png"
+      , contact = "Franz-Josef Breuer"
+      , address = "Jahnstraße 9"
+      , email = ""
+      , description = ""
+      }
+      , { title = "Schützenbruderschaft St. Lambertus Birgelen"
+      , url = ""
+      , logo = "birgelen.png"
+      , contact = "Karl-Heinz Dohmen"
+      , address = "Mahrstraße 21"
+      , email = ""
+      , description = ""
+      }
+      , { title = "Theatergruppe Birgelen"
+      , url = ""
+      , logo = "birgelen.png"
+      , contact = "Astrid Bärmann "
+      , address = "Am Kulenberg 8"
+      , email = ""
+      , description = ""
+      }
+      , { title = "Trommler- und Pfeifercorps Birgelen"
+      , url = ""
+      , logo = "birgelen.png"
+      , contact = "Andre Honigs"
+      , address = ""
+      , email = ""
+      , description = ""
+      }
+      , { title = "TTC Birgelen-Ophoven"
+      , url = ""
+      , logo = "birgelen.png"
+      , contact = "Christian Erdner"
+      , address = "Kreuzbuschstraße 8"
       , email = "ePost"
       , description = "Beschreibung"
       }
+      , { title = "VdK Ortsverband Wassenberg-Birgelen"
+      , url = "http://www.vdk.de/kv-heinsberg/ID3584"
+      , logo = "birgelen.png"
+      , contact = "Günter Kirstein"
+      , address = "Mittlerer Weg 48"
+      , email = ""
+      , description = ""
+      }
+      , { title = "Messdiener"
+      , url = ""
+      , logo = "birgelen.png"
+      , contact = ""
+      , address = ""
+      , email = ""
+      , description = ""
+      }
+      , { title = "Grundschule Birgelen"
+      , url = ""
+      , logo = "birgelen.png"
+      , contact = ""
+      , address = ""
+      , email = ""
+      , description = ""
+      }
+      , { title = "Kindergarten St. Lambertus"
+      , url = "http://www.kindergarten-birgelen.de/"
+      , logo = "birgelen.png"
+      , contact = ""
+      , address = ""
+      , email = ""
+      , description = ""
+      }
+      , { title = "Birgelen Veterans Association"
+      , url = "http://www.birgelenvets.org/"
+      , logo = "birgelen.png"
+      , contact = ""
+      , address = ""
+      , email = ""
+      , description = ""
+      }
     ]
+
