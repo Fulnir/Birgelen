96c08da26d39f822dda8371676bf6fd93100832d
Edwin Bühler <edbuehler@icloud.com>
2018-02-24 10:42:54 +0100

    deliver config updated

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule BirgelenApp.Mixfile do
   def project do
     [
       app: :birgelen_app,
-      version: "0.0.2",
-      elixir: "~> 1.6",
+      version: "0.0.3",
+      elixir: "~> 1.5.2",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
       start_permanent: Mix.env == :prod,
